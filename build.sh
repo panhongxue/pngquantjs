72ddbb9fe36f3d15821ed47ec763c930f44fa433
panhongxue <panhongxue@360.cn>
2023-09-27 17:42:06 +0800

    opt：内存限制，增大到：1G

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ emmake make
 cd $DIR/deps/pngquant
 
 # start configuring script
-emconfigure ./configure --disable-sse --with-libpng=/usr/include --extra-cflags="-sUSE_LIBPNG=1 -sTOTAL_MEMORY=104857600 --proxy-to-worker " #--enable-debug
+emconfigure ./configure --disable-sse --with-libpng=/usr/include --extra-cflags="-sUSE_LIBPNG=1 -sTOTAL_MEMORY=1048576000 --proxy-to-worker " #--enable-debug
 
 # At this point just make sure that config.mk file points correct version of
 # libpng
